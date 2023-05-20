975fde188d4353523a9b64976dff256514b10475
Sahejdeep Singh <bboysahej@gmail.com>
2023-05-20 16:54:54 -0700

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+interface Product {
+  name: string;
+  link: string;
+  specifications?: ProductSpecification[];
+  imageLink?: string;
+}
+
+interface ProductResponse {
+  productName: string;
+}
+
+interface ProductSpecification {
+  key: string;
+  value: string;
+  imageLink: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +25,85 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'staples-compare';
+  productLink!: string;
+  products: Product[] = [];
+  isLoading = false;
+
+  constructor(private http: HttpClient) {}
+
+  addProduct(): void {
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
+  
+    this.http.post<ProductResponse>('http://localhost:5063/api/products/scrape', JSON.stringify(this.productLink), httpOptions).subscribe(
+      response => {
+        const productName = response.productName;
+        this.products.push({ name: productName, link: this.productLink });
+        this.productLink = ''; // Clear the input field
+      },
+      error => {
+        console.error('Error scraping product:', error);
+      }
+    );
+  }
+
+  compareProducts(): void {
+
+    this.isLoading = true;
+
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
+
+    const productLinks = this.products.map(product => product.link);
+
+    this.products.forEach((product, index) => {
+      this.http.post<ProductSpecification[]>('http://localhost:5063/api/products/scrape-specifications', JSON.stringify([product.link]), httpOptions).subscribe(
+        specifications => {
+          product.specifications = specifications;
+          product.imageLink = specifications[0].imageLink;
+
+          if (index === this.products.length - 1) {
+            this.isLoading = false;
+          }
+        },
+        error => {
+          console.error('Error scraping specifications:', error);
+          this.isLoading = false;
+        }
+      );
+    });
+  }
+
+
+  getProductKeys(): string[] {
+    let maxKeysProduct: Product | undefined;
+    let maxKeysCount = 0;
+  
+    for (const product of this.products) {
+      if (product.specifications && product.specifications.length > maxKeysCount) {
+        maxKeysProduct = product;
+        maxKeysCount = product.specifications.length;
+      }
+    }
+  
+    return maxKeysProduct ? maxKeysProduct.specifications!.map(spec => spec.key) : [];
+  }
+  
+  getProductSpecificationValue(product: Product, key: string): string {
+    if (product.specifications && product.specifications.length > 0) {
+      const specification = product.specifications.find(spec => spec.key === key);
+      return specification ? specification.value : '';
+    }
+    return '';
+  }
+
+  removeProduct(product: Product): void {
+    const index = this.products.indexOf(product);
+    if (index !== -1) {
+      this.products.splice(index, 1);
+    }
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    FormsModule,
+    HttpClientModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
